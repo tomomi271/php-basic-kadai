a352fa390e6af8252481a60dd66289c1b53ce08c
tomomi271 <tomo7701@gmail.com>
2025-12-30 15:39:48 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>7章の課題</title>
+</head>
+<body>
+   <p>
+       <?php
+      //  連記配列に値を代入する
+       $user_name = ['name'=> 'onion','price' => 200 ,'weight' => 160];
+
+      //  連記配列の値を出力する
+      print_r($user_name);
+
+      ?>
+   </p>
+</body>
+</html>
